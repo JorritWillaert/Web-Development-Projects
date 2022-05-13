3b45b9173188011f819257c32d9373ffa747d176
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-13 17:12:53 +0200

    Toggle add task

--- angular-crash-course/angular-crash/src/app/components/add-task/add-task.component.ts
+++ angular-crash-course/angular-crash/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 import { Task } from '../../Task'
 
 @Component({
@@ -11,8 +13,12 @@ export class AddTaskComponent implements OnInit {
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
   }
