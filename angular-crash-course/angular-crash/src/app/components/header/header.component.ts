cea0ada317eadd78ae3806f9a03ed7e7886c11d6
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-13 17:08:49 +0200

    Toggle the color and text of the button

--- angular-crash-course/angular-crash/src/app/components/header/header.component.ts
+++ angular-crash-course/angular-crash/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,16 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
   title: string = 'Task Tracker';
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask()
   }
 
 }
