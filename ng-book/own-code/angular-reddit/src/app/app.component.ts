b38b0f54229e04264c4c3e802f50b4b8cc89d52b
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-15 17:09:29 +0200

    Add button and pass arguments

--- ng-book/own-code/angular-reddit/src/app/app.component.ts
+++ ng-book/own-code/angular-reddit/src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`)
+    return false;
+  }
 }
