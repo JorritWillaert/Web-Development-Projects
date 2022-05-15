c969ebf5f17042a7594b27e53bdc37e526f7ca5b
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-15 17:31:18 +0200

    Add some sample entry - upvoting and downvoting not yet working

---
+++ ng-book/own-code/angular-reddit/src/app/article/article.component.ts
@@ -0,0 +1,31 @@
+import { Component, HostBinding, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor() {
+    this.title = 'Angular';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+  voteUp() {
+    this.votes += 1;
+  }
+
+  voteDown() {
+    this.votes -= 1;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
