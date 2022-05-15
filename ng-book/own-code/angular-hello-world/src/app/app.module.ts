1c71229d30d7aad6d487447ef6149a8208674c33
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-15 16:28:46 +0200

    Add new components for creating multiple list items with names

--- ng-book/own-code/angular-hello-world/src/app/app.module.ts
+++ ng-book/own-code/angular-hello-world/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
