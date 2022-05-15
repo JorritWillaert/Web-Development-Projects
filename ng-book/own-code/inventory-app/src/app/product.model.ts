dc53c6a65d0b3f52db4a76a4460678a5515f6580
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-15 20:19:28 +0200

    Create new app for inventories

---
+++ ng-book/own-code/inventory-app/src/app/product.model.ts
@@ -0,0 +1,11 @@
+/**
+* Provides a `Product` object
+*/
+export class Product {
+    constructor(
+        public sku: string,
+        public name: string,
+        public imageUrl: string,
+        public department: string[],
+        public price: number) {}
+}
