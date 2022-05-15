6da99700afb94f8dc36307a304734ffe29c98366
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-15 20:32:48 +0200

    Add more products

--- ng-book/own-code/inventory-app/src/app/app.component.ts
+++ ng-book/own-code/inventory-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'inventory-app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-app';
+  products: Product[];
+  
+  constructor() {
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99)
+      ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log(`Product ${product} was selected.`);
+  }
 }
