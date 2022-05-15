947a8ac36a961f6dcb496e023d3303a4d31fe603
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-15 19:26:11 +0200

    Dynamically pass the articles

--- ng-book/own-code/angular-reddit/src/app/app.component.ts
+++ ng-book/own-code/angular-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[]
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('Vue', 'http://vue.com', 2),
+      new Article('React', 'http://react.com', 1)
+    ];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`)
     return false;
--- ng-book/own-code/angular-reddit/src/app/article/article.component.ts
+++ ng-book/own-code/angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit, Input } from '@angular/core';
 import { Article } from './article.model'
 
 @Component({
@@ -8,13 +8,9 @@ import { Article } from './article.model'
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article
+  @Input() article: Article
 
-  constructor() {
-    this.article = new Article(
-      'Angular','http://angular.io', 10
-    );
-  }
+  constructor() { }
 
   voteUp(): boolean {
     this.article.voteUp();
