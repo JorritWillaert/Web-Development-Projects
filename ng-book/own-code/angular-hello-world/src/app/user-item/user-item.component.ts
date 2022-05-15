24dcd2845b226a12dae19281068f54640f2d5c15
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-15 16:44:49 +0200

    Use user-list-item instead of raw input

--- ng-book/own-code/angular-hello-world/src/app/user-item/user-item.component.ts
+++ ng-book/own-code/angular-hello-world/src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -6,11 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
-  name: string;
+  @Input() name: string;
 
-  constructor() {
-    this.name = 'Felipe';  
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
