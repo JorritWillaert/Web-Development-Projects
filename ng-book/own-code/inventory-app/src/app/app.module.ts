cdb57f68fd8c2d989b344e8a16454e59f7efbde2
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-16 21:18:16 +0200

    Add product-department

--- ng-book/own-code/inventory-app/src/app/app.module.ts
+++ ng-book/own-code/inventory-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductRowComponent } from './product-row/product-row.component';
 import { ProductImageComponent } from './product-image/product-image.component';
 import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { PriceDisplayComponent } from './price-display/price-display.component';
     ProductsListComponent,
     ProductRowComponent,
     ProductImageComponent,
-    PriceDisplayComponent
+    PriceDisplayComponent,
+    ProductDepartmentComponent
   ],
   imports: [
     BrowserModule
---
+++ ng-book/own-code/inventory-app/src/app/product-department/product-department.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'product-department',
+  templateUrl: './product-department.component.html',
+  styleUrls: ['./product-department.component.css']
+})
+export class ProductDepartmentComponent {
+  @Input() product: Product;
+
+}
