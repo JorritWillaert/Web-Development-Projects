d6857773b2b8e1dcda2043dc9f301192095d1ea7
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-16 08:33:50 +0200

    Add clicking and isSelected functionality to products-list

--- ng-book/own-code/inventory-app/src/app/products-list/products-list.component.ts
+++ ng-book/own-code/inventory-app/src/app/products-list/products-list.component.ts
@@ -15,4 +15,16 @@ export class ProductsListComponent {
     this.onProductSelected = new EventEmitter();
   }
 
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
+
 }
