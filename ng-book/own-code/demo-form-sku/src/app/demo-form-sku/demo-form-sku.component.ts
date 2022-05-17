c03afa40ca98bafb21e3a333339b7fdf7d170f2e
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-17 21:28:44 +0200

    Try out 'prettier'

--- ng-book/own-code/demo-form-sku/src/app/demo-form-sku/demo-form-sku.component.ts
+++ ng-book/own-code/demo-form-sku/src/app/demo-form-sku/demo-form-sku.component.ts
@@ -3,17 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-demo-form-sku',
   templateUrl: './demo-form-sku.component.html',
-  styleUrls: ['./demo-form-sku.component.css']
+  styleUrls: ['./demo-form-sku.component.css'],
 })
 export class DemoFormSkuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(form: any): void {
     console.log('You submitted value:', form);
   }
-
 }
