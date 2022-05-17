857a63c3d1c81ff48e660ea510fc71e3ed33d389
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-17 20:47:49 +0200

    Create new project for forms and import FormsModule & ReactiveFormsModule

---
+++ ng-book/own-code/demo-form-sku/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
