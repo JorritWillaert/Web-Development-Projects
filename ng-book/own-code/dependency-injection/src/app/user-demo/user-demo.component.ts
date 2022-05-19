a07a27ec4785dce848b314d827110436c980c9f6
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-19 19:53:00 +0200

    Do manual dependency injection

--- ng-book/own-code/dependency-injection/src/app/user-demo/user-demo.component.ts
+++ ng-book/own-code/dependency-injection/src/app/user-demo/user-demo.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ReflectiveInjector } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-demo',
   templateUrl: './user-demo.component.html',
-  styleUrls: ['./user-demo.component.css']
+  styleUrls: ['./user-demo.component.css'],
 })
-export class UserDemoComponent implements OnInit {
+export class UserDemoComponent {
+  userName: string;
+  userService: UserService;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    const injector: any = ReflectiveInjector.resolveAndCreate([UserService]);
+    this.userService = injector.get(UserService);
   }
 
+  signIn(): void {
+    this.userService.setUser({
+      name: 'Nate Murray',
+    });
+
+    this.userName = this.userService.getUser().name;
+    console.log('User name is: ', this.userName);
+  }
 }
