a16c8e9e239b41fcdcaaf18b2b2b5c0cf4e8cd8d
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-20 17:13:38 +0200

    Add code for HTTP requests

--- ng-book/own-code/dependency-injection/src/app/analytics-demo/analytics-demo.module.ts
+++ ng-book/own-code/dependency-injection/src/app/analytics-demo/analytics-demo.module.ts
@@ -3,20 +3,28 @@ import { CommonModule } from '@angular/common';
 import { Metric, AnalyticsImplementation } from './analytics-demo.interface';
 import { AnalyticsService } from '../services/analytics.service';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   providers: [
+    { provide: 'API_URL', useValue: 'http://devserver.com' },
+    // `AnalyticsService` is the _token_ we use to inject
+    // note, the token is the class, but it's just used as an identifier!
     {
-      // `AnalyticsService` is the _token_ we use to inject
-      // note, the token is the class, but it's just used as an identifier!
       provide: AnalyticsService,
+
+      deps: [HttpClient, "API_URL"],
+
       // useFactory is a function - whatever is returned from this function
       // will be injected
-      useFactory() {
+      useFactory(http: HttpClient, apiUrl: string) {
         // create an implementation that will log the event
         const loggingImplementation: AnalyticsImplementation = {
           recordEvent: (metric: Metric): void => {
             console.log('The metric is:', metric);
+            console.log("Sending to: ", apiUrl);
+            // ... You'd send the metric using http here ...
           },
         };
         // create our new `AnalyticsService` with the implementation
