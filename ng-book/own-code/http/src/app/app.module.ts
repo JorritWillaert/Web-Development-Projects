5a82b065840a052c25d022b761c38243c327ebdb
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-22 19:51:09 +0200

    Create the encompassing youtube search component

--- ng-book/own-code/http/src/app/app.module.ts
+++ ng-book/own-code/http/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { SimpleHttpComponent } from './simple-http/simple-http.component';
 import { youTubeSearchInjectables } from './youtube-search/youtube-search.injectables';
 import { SearchBoxComponent } from './youtube-search/search-box/search-box.component';
 import { SearchResultComponent } from './youtube-search/search-result/search-result.component';
+import { YoutubeSearchComponent } from './youtube-search/youtube-search/youtube-search.component';
 
 @NgModule({
-  declarations: [AppComponent, SimpleHttpComponent, SearchBoxComponent, SearchResultComponent],
+  declarations: [AppComponent, SimpleHttpComponent, SearchBoxComponent, SearchResultComponent, YoutubeSearchComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [youTubeSearchInjectables],
   bootstrap: [AppComponent],
