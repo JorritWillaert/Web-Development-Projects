323c911e2e576fa6746eed28bbedaf5a1887be82
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-22 19:45:25 +0200

    Add the search result

--- ng-book/own-code/http/src/app/app.module.ts
+++ ng-book/own-code/http/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { SimpleHttpComponent } from './simple-http/simple-http.component';
 import { youTubeSearchInjectables } from './youtube-search/youtube-search.injectables';
 import { SearchBoxComponent } from './youtube-search/search-box/search-box.component';
+import { SearchResultComponent } from './youtube-search/search-result/search-result.component';
 
 @NgModule({
-  declarations: [AppComponent, SimpleHttpComponent, SearchBoxComponent],
+  declarations: [AppComponent, SimpleHttpComponent, SearchBoxComponent, SearchResultComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [youTubeSearchInjectables],
   bootstrap: [AppComponent],
---
+++ ng-book/own-code/http/src/app/youtube-search/search-result/search-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { SearchResult } from '../search-result.model';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.css'],
+})
+export class SearchResultComponent implements OnInit {
+  @Input() result: SearchResult;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
