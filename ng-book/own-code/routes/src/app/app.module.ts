c4a261a3d92e56029fb8c75cfd0f87148ae4b834
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-22 20:20:58 +0200

    Add three basic components

--- ng-book/own-code/routes/src/app/app.module.ts
+++ ng-book/own-code/routes/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   // basic routes
@@ -30,7 +33,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
