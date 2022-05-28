42ae11ace44961e9e2f6295c0af8357ee932e0e0
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-28 21:24:48 +0200

    Work further on the typescript of search component

--- ng-book/own-code/music/src/app/search/search.component.ts
+++ ng-book/own-code/music/src/app/search/search.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SpotifyServiceService } from '../services/spotify-service.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  query: string;
+  results: Object | null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private spotify: SpotifyServiceService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParams.subscribe((params) => {
+      this.query = params['query'] || '';
+    });
   }
 
+  ngOnInit(): void {}
+
+  search() : void {
+    console.log('this.query', this.query);
+    if (!this.query) {
+      return;
+    }
+
+    this.spotify.searchTrack(this.query).subscribe((res: any) => this.renderResults(res));
+  }
+
+  renderResults(res: any): void {
+    this.results = null;
+    if (res && res.tracks && res.tracks.items) {
+      this.results = res.tracks.items;
+    }
+  }
 }
