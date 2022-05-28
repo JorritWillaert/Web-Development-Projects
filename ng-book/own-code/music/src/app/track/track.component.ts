68e665eadcafd630d469f99b85e64c12b98fb09e
JorritWillaert <jorritwillaert98@gmail.com>
2022-05-28 22:18:22 +0200

    Add Typescript code for track component

--- ng-book/own-code/music/src/app/track/track.component.ts
+++ ng-book/own-code/music/src/app/track/track.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SpotifyServiceService } from '../services/spotify-service.service';
+import {Location} from '@angular/common';
+
 
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
-  styleUrls: ['./track.component.css']
+  styleUrls: ['./track.component.css'],
 })
 export class TrackComponent implements OnInit {
+  id: string;
+  track: Object;
 
-  constructor() { }
+  constructor(
+    private spotify: SpotifyServiceService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.spotify.getTrack(this.id).subscribe((res: any) => this.renderTrack(res));
   }
 
+  back(): void {
+    this.location.back();
+  }
+
+  renderTrack(res: any): void {
+    this.track = res;
+  }
 }
