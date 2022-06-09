cde1c77be25255d5b1aef94d3b300d4ac34462ac
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-09 16:02:45 +0200

    Add new project and add simple classes

---
+++ ng-book/own-code/chat-app/src/app/user/user.model.ts
@@ -0,0 +1,12 @@
+import { uuid } from '../util/uuid';
+
+/**
+ * A User represents an agent that sends messages
+ */
+export class User {
+  id: string;
+
+  constructor(public name: string, public avatarSrc: string) {
+    this.id = uuid();
+  }
+}
