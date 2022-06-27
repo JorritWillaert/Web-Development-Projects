fdbef78822c161e5622a0f3172331dd877cde71e
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-27 13:25:05 +0200

    Add the actions

---
+++ ng-book/own-code/redux/src/app/counter.reducer.ts
@@ -0,0 +1,19 @@
+import { INCREMENT, DECREMENT } from './counter.actions';
+import { AppState } from './app.state';
+
+const initialState: AppState = { counter: 0 };
+
+// Create our reducer that will handle changes to the state
+export const counterReducer: Reducer<AppState> = (
+  state: AppState = initialState,
+  action: Action
+): AppState => {
+  switch (action.type) {
+    case INCREMENT:
+      return Object.assign({}, state, { counter: state.counter + 1 });
+    case DECREMENT:
+      return Object.assign({}, state, { counter: state.counter - 1 });
+    default:
+      return state;
+  }
+};
