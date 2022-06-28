09d35d99bf2660f0e79da280ed143e84fb8f41a0
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-28 20:14:46 +0200

    Try out scripts and more packages

--- node-js-tutorial/03-npm/index.js
+++ node-js-tutorial/03-npm/index.js
@@ -1,1 +1,8 @@
-console.log('testing!')
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
+
+console.log('Hello');
+
+console.log(uuid());
