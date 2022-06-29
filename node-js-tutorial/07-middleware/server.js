4db4f1d18c6afae66c67b284b43d23fb203b75ef
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 11:23:44 +0200

    Finalize some more middleware

--- node-js-tutorial/07-middleware/server.js
+++ node-js-tutorial/07-middleware/server.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const cors = require('cors');
+const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 //  Custom middleware logger
 app.use(logger);
 
 // Cross Origin Resource Sharing
-const whitelist = ['https://winergie.be', 'https://www.google.com', 'http://localhost:3500'];
+const whitelist = [
+  "https://winergie.be",
+  "https://www.google.com",
+  "http://localhost:3500",
+];
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) != -1) {
+    if (whitelist.indexOf(origin) != -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  optionsSuccessStatus: 200
-}
+  optionsSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
 // Built-in middleware to handle urlencoded data
@@ -73,8 +78,17 @@ const three = (req, res, next) => {
 
 app.get("/chain(.html)?", [one, two, three]);
 
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type('txt').send("404 Not Found");
+  }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
