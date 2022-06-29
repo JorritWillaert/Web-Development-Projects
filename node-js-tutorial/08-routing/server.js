3c1bd30cc5bbe13743a1289001f9bf0c809c5c76
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 11:25:27 +0200

    Initial files for chapter 8 routing

---
+++ node-js-tutorial/08-routing/server.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const PORT = process.env.PORT || 3500;
+
+//  Custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = [
+  "https://winergie.be",
+  "https://www.google.com",
+  "http://localhost:3500",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) != -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// Built-in middleware to handle urlencoded data
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("^/$|/index(.html)?", (req, res) => {
+  //res.sendFile('./views/index.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/new-page(.html)?", (req, res) => {
+  //res.sendFile('./views/index.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html"); // 302 by default, we want a 301 (permanent move)
+});
+
+// Route handlers
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("Attempted to load hello.txt");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World!");
+  }
+);
+
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+
+const three = (req, res, next) => {
+  console.log("three");
+  next();
+};
+
+app.get("/chain(.html)?", [one, two, three]);
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type('txt').send("404 Not Found");
+  }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
