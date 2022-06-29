d92ea9acfae7155d620ead818e64a4b3d993a462
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 13:45:25 +0200

    Path correction

--- node-js-tutorial/09-mvc-rest-api/routes/api/employees.js
+++ node-js-tutorial/09-mvc-rest-api/routes/api/employees.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const employeesController = require('../../controllers/employeesController');
+const employeesController = require('../model/employeesController');
 
 router
   .route("/")
