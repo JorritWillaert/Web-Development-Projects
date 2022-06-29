3ea414db5ba60d9944157735d688a7f5dfaba973
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 14:04:03 +0200

    Finalize the other code for in the controller

--- node-js-tutorial/09-mvc-rest-api/controllers/employeesController.js
+++ node-js-tutorial/09-mvc-rest-api/controllers/employeesController.js
@@ -41,18 +41,42 @@ const updateEmployee = (req, res) => {
   if (req.body.lastname) {
     employee.lastname = req.body.lastname;
   }
-  const filteredArray = data.employees.filter(emp => emp.id != parseInt(req.body.id));
+  const filteredArray = data.employees.filter(
+    (emp) => emp.id != parseInt(req.body.id)
+  );
   const unsortedArray = [...filteredArray, employee];
-  data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1: 0));
+  data.setEmployees(
+    unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+  );
   res.json(data.employees);
 };
 
 const deleteEmployee = (req, res) => {
-  res.json({ id: req.body.id });
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.body.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found` });
+  }
+  const filteredArray = data.employees.filter(
+    (emp) => emp.id != parseInt(req.body.id)
+  );
+  data.setEmployees([...filteredArray]);
+  res.json(data.employees);
 };
 
 const getEmployee = (req, res) => {
-  res.json({ id: req.params.id });
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.body.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found` });
+  }
+  res.json(employee);
 };
 
 module.exports = {
