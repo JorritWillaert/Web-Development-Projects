58935a435c7ee7be49da507f376a841ccbbb0d99
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 13:16:53 +0200

    Add route for specific ID

--- node-js-tutorial/08-routing/routes/api/employees.js
+++ node-js-tutorial/08-routing/routes/api/employees.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
 const data = {};
 data.employees = require("../../data/employees.json");
 
@@ -24,5 +23,9 @@ router
   .delete((req, res) => {
     res.json({ id: req.body.id });
   });
+
+router.route("/:id").get((req, res) => {
+  res.json({ id: req.params.id });
+});
 
 module.exports = router;
