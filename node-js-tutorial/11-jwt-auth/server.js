cf92066647ea814a107569bab3e395951a47b402
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 18:37:12 +0200

    Add refresh token with cookie

--- node-js-tutorial/11-jwt-auth/server.js
+++ node-js-tutorial/11-jwt-auth/server.js
@@ -20,12 +20,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
+app.use(cookieParser())
+
 // Serve static files
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+app.use('/refresh', require('./routes/refresh'));
 
 // All other pages will require verification!
 app.use(verifyJWT);
