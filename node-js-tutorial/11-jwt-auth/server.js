6869481247af7f7f0488b8c1d1113d855da6e951
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 16:19:46 +0200

    Add initial files for chapter 11

---
+++ node-js-tutorial/11-jwt-auth/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const PORT = process.env.PORT || 3500;
+
+//  Custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// Built-in middleware to handle urlencoded data
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+// Serve static files
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/employees", require("./routes/api/employees"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
