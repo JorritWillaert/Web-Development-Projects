8cefc15571ffbc89d8c61a96552cdbd3ba5617f6
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 09:05:17 +0200

    Switch on the file extension

--- node-js-tutorial/05-web-server/server.js
+++ node-js-tutorial/05-web-server/server.js
@@ -1,11 +1,45 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
 
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-class Emitter extends EventEmitter {};
+const logEvents = require("./logEvents");
+const EventEmitter = require("events");
+class Emitter extends EventEmitter {}
 
 // Initialize object
 const myEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  const extension = path.extname(req.url);
+  let contentType;
+
+  switch (extension) {
+    case '.css':
+        contentType = 'text/css';
+        break;
+    case '.js':
+        contentType = 'text/javascript';
+        break;
+    case '.json':
+        contentType = 'application/json';
+        break;
+    case '.jpg':
+        contentType = 'image/jpeg';
+        break;
+    case '.png':
+        contentType = 'image/png';
+        break;
+    case '.txt':
+        contentType = 'text/plain';
+        break;
+    default:
+        contentType = 'text/html';
+}
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
