832b303fc5801cf67586aaa03ef291e29b57a5c2
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 09:13:47 +0200

    Build up filepath further

--- node-js-tutorial/05-web-server/server.js
+++ node-js-tutorial/05-web-server/server.js
@@ -19,27 +19,48 @@ const server = http.createServer((req, res) => {
   let contentType;
 
   switch (extension) {
-    case '.css':
-        contentType = 'text/css';
-        break;
-    case '.js':
-        contentType = 'text/javascript';
-        break;
-    case '.json':
-        contentType = 'application/json';
-        break;
-    case '.jpg':
-        contentType = 'image/jpeg';
-        break;
-    case '.png':
-        contentType = 'image/png';
-        break;
-    case '.txt':
-        contentType = 'text/plain';
-        break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".jpg":
+      contentType = "image/jpeg";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".txt":
+      contentType = "text/plain";
+      break;
     default:
-        contentType = 'text/html';
-}
+      contentType = "text/html";
+  }
+
+  // Horrible statement, but simply following the tutorial...
+  let filePath = contentType === 'text/html' && req.url === '/' ? path.join(__dirname, 'views', 'index.html')
+    : contentType === 'text/html' && req.url.slice(-1) === '/' ? path.join(__dirname, 'views', req.url, 'index.html')
+    : contentType === 'text/html' ? path.join(__dirname, 'views', req.url)
+    : path.join(__dirname, req.url);
+
+  // Makes .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== '/') {
+    filePath += '.html';
+  }
+
+  const fileExists = fs.existsSync(filePath);
+  
+  if (fileExists) {
+    // Serve the file
+  } else {
+    // 404
+    // 301 redirect
+    console.log(path.parse(filePath));
+  }
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
