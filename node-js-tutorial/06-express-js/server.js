306c8cc3a3bca08ea9e48a1bc3fd120e456a896d
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 10:07:25 +0200

    Use route handlers in Express JS

--- node-js-tutorial/06-express-js/server.js
+++ node-js-tutorial/06-express-js/server.js
@@ -3,14 +3,47 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
-app.get('^/$|/index.html', (req, res) => {
+app.get('^/$|/index(.html)?', (req, res) => {
   //res.sendFile('./views/index.html', { root: __dirname });
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.get('/new-page.html', (req, res) => {
+app.get('/new-page(.html)?', (req, res) => {
   //res.sendFile('./views/index.html', { root: __dirname });
   res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 });
+
+app.get('/old-page(.html)?', (req, res) => {
+  res.redirect(301, '/new-page.html'); // 302 by default, we want a 301 (permanent move)
+});
+
+// Route handlers
+app.get('/hello(.html)?', (req, res, next) => {
+  console.log('Attempted to load hello.txt');
+  next();
+}, (req, res) => {
+  res.send('Hello World!');
+})
+
+const one = (req, res, next) => {
+  console.log('one');
+  next();
+}
+
+const two = (req, res, next) => {
+  console.log('two');
+  next();
+}
+
+const three = (req, res, next) => {
+  console.log('three');
+  next();
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.get('/*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
