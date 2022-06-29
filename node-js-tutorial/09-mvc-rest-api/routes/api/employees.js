2ecf6bbb7f4bd6f1c9cff39886b3ebca02056d75
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 14:09:34 +0200

    Bugfix

--- node-js-tutorial/09-mvc-rest-api/routes/api/employees.js
+++ node-js-tutorial/09-mvc-rest-api/routes/api/employees.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const employeesController = require('../model/employeesController');
+const employeesController = require('../../controllers/employeesController');
 
 router
   .route("/")
