aa828ff822f8e7c9fe124d15198930e5a6d8dfef
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 08:52:31 +0200

    Add initial files for the web server

---
+++ node-js-tutorial/05-web-server/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter {};
+
+// Initialize object
+const myEmitter = new Emitter();
