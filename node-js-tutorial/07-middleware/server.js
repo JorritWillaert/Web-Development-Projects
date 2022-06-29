2957dce30799656492cbdcbcd64317e0e5956950
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-29 11:07:42 +0200

    Add some sites that are allowed for CORS

--- node-js-tutorial/07-middleware/server.js
+++ node-js-tutorial/07-middleware/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require('cors');
 const { logger } = require("./middleware/logEvents");
 const PORT = process.env.PORT || 3500;
 
 //  Custom middleware logger
 app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = ['https://winergie.be', 'https://www.google.com', 'http://localhost:3500'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) != -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 // Built-in middleware to handle urlencoded data
 app.use(express.urlencoded({ extended: false }));
