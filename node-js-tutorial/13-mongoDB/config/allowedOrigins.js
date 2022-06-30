e1e0b88f71b61c53ca9f1807a2095672eacd7217
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-30 17:13:09 +0200

    Initial files for chapter 13 with MongoDB

---
+++ node-js-tutorial/13-mongoDB/config/allowedOrigins.js
@@ -0,0 +1,3 @@
+const allowedOrigins = ["http://winergie.com", "http://localhost:3500"];
+
+module.exports = allowedOrigins;
---
+++ node-js-tutorial/13-mongoDB/controllers/logoutController.js
@@ -0,0 +1,54 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const handleLogout = async (req, res) => {
+  // On client, also delete the access token
+
+  const cookies = req.cookies;
+  if (!cookies?.jwt) {
+    return res.sendStatus(204); // No contents
+  }
+  const refreshToken = cookies.jwt;
+
+  // Is refresh token in DB?
+  const foundUser = usersDB.users.find(
+    (person) => person.refreshToken == refreshToken
+  );
+
+  if (!foundUser) {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    return res.sendStatus(204); // Forbidden
+  }
+
+  // Delete refresh token in DB
+  const otherUsers = usersDB.users.filter(
+    (person) => person.refreshToken !== foundUser.refreshToken
+  );
+  const currentUser = { ...foundUser, refreshToken: "" };
+  usersDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "model", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    maxAge: 24 * 60 * 60 * 1000,
+  }); // secure: true - only serves on https (for production)
+  res.sendStatus(204);
+};
+
+module.exports = { handleLogout };
