4e69018875214098066b6bfa4d24df844c202394
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-30 18:07:05 +0200

    Update the controller to use the DB

--- node-js-tutorial/14-mongoose-data-models/controllers/registerController.js
+++ node-js-tutorial/14-mongoose-data-models/controllers/registerController.js
@@ -1,11 +1,4 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-const fsPromises = require("fs").promises;
-const path = require("path");
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) => {
@@ -16,25 +9,21 @@ const handleNewUser = async (req, res) => {
       .json({ message: "Username and password are required." });
   }
   // Check for duplicate usernames in the DB
-  const duplicate = usersDB.users.find((person) => person.username === user);
+  const duplicate = await User.findOne({ username: user }).exec();
   if (duplicate) {
     return res.sendStatus(409); // Conflict
   }
   try {
     // Encrypt password
     const hashedPassword = await bcrypt.hash(pwd, 10);
-    // Store new user
-    const newUser = {
+
+    // Create and store new user
+    const result = await User.create({
       username: user,
-      roles: { User: 2001 },
       password: hashedPassword,
-    };
-    usersDB.setUsers([...usersDB.users, newUser]);
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users)
-    );
-    console.log(usersDB.users);
+    });
+    console.log(result);
+
     res.status(201).json({ success: `New user ${user} created!` });
   } catch (err) {
     res.status(500).json({ message: err.message });
--- node-js-tutorial/14-mongoose-data-models/model/User.js
+++ node-js-tutorial/14-mongoose-data-models/model/User.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
     required: true,
   },
   roles: {
-    user: {
+    User: {
       type: Number,
       default: 2001,
     },
