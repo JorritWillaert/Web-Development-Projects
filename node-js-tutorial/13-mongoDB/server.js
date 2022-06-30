014d57346f68bb6c166849cb1e3bd20097512c5c
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-30 17:24:20 +0200

    Set up code for connection with Mongo DB

--- node-js-tutorial/13-mongoDB/server.js
+++ node-js-tutorial/13-mongoDB/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -8,7 +9,12 @@ const errorHandler = require("./middleware/errorHandler");
 const verifyJWT = require("./middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
 const credentials = require('./middleware/credentials');
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
+
+// Connect to Mongo DB
+connectDB();
 
 //  Custom middleware logger
 app.use(logger);
@@ -56,4 +62,7 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once('open', () => {
+  console.log('Connected to Mongo DB')
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
