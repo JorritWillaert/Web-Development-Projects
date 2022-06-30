530e280340862d1ebfe0d72d88013e724c9c4c09
JorritWillaert <jorritwillaert98@gmail.com>
2022-06-30 12:58:46 +0200

    Initial files for chapter 12 authorization roles

---
+++ node-js-tutorial/12-authorization-roles/server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const credentials = require('./middleware/credentials');
+const PORT = process.env.PORT || 3500;
+
+//  Custom middleware logger
+app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirements
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// Built-in middleware to handle urlencoded data
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+// Serve static files
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
+
+// All other pages will require verification!
+app.use(verifyJWT);
+
+app.use(cookieParser());
+
+app.use("/employees", require("./routes/api/employees"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
