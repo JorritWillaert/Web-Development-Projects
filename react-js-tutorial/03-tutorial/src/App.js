1ff89009e68724c2cc6ab70190a69205c9159a84
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-01 10:21:28 +0200

    Create new functional components

--- react-js-tutorial/03-tutorial/src/App.js
+++ react-js-tutorial/03-tutorial/src/App.js
@@ -1,27 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 function App() {
   const name = "Jorrit";
 
-  const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Dave'];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Hello {handleNameChange()}!
-        </p>
-        {/* <p>{name}</p> */}
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
