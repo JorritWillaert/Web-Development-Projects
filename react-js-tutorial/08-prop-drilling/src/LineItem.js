6d9762cbe395233b8c53599f9472613a6957f200
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-01 17:40:26 +0200

    More formatting by creating a LineItem component

---
+++ react-js-tutorial/08-prop-drilling/src/LineItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+const LineItem = ({item, handleCheck, handleDelete}) => {
+  return (
+    <li className="item" key={item.id}>
+      <input
+        type="checkbox"
+        onChange={() => handleCheck(item.id)}
+        checked={item.checked}
+      />
+      <label
+        style={item.checked ? { textDecoration: "line-through" } : null}
+        onDoubleClick={() => handleCheck(item.id)}
+      >
+        {item.item}
+      </label>
+      <FaTrashAlt
+        onClick={() => handleDelete(item.id)}
+        role="button"
+        tabIndex="0"
+      />
+    </li>
+  );
+};
+
+export default LineItem;
