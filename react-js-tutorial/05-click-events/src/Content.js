760dbf136dcdc304d4ed56390298da40d732fe76
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-01 10:46:26 +0200

    Add some event listeners

--- react-js-tutorial/05-click-events/src/Content.js
+++ react-js-tutorial/05-click-events/src/Content.js
@@ -7,9 +7,24 @@ const Content = () => {
     return names[int];
   };
 
+  const handleClick = () => {
+    console.log("You clicked it!");
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked!`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
-      <p>Hello {handleNameChange()}</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={() => handleClick2('Jorrit')}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   );
 };
