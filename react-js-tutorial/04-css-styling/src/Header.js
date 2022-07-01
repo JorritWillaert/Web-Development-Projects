49fa9a040b2da3492b6d489fc40f62be55fad2a9
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-01 10:34:22 +0200

    Add some global styling

--- react-js-tutorial/04-css-styling/src/Header.js
+++ react-js-tutorial/04-css-styling/src/Header.js
@@ -1,12 +1,8 @@
 import React from "react";
 
 const Header = () => {
-  const headerStyle = {
-    backgroundColor: "mediumblue",
-    color: "#fff",
-  };
   return (
-    <header style={headerStyle}>
+    <header>
       <h1>Groceries List</h1>
     </header>
   );
