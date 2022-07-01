c3f9e9920355c5d8fc40d9ee246dcdbc0cc13533
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-01 17:42:29 +0200

    Add an aria-label

--- react-js-tutorial/08-prop-drilling/src/LineItem.js
+++ react-js-tutorial/08-prop-drilling/src/LineItem.js
@@ -19,6 +19,7 @@ const LineItem = ({item, handleCheck, handleDelete}) => {
         onClick={() => handleDelete(item.id)}
         role="button"
         tabIndex="0"
+        aria-label={`Delete ${item.items}`}
       />
     </li>
   );
