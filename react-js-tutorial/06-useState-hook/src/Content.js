d1e575df76a671cdb79f6a13fb459baa005985a5
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-01 11:01:47 +0200

    Start working with state

--- react-js-tutorial/06-useState-hook/src/Content.js
+++ react-js-tutorial/06-useState-hook/src/Content.js
@@ -1,30 +1,35 @@
 import React from "react";
+import { useState } from "react";
 
 const Content = () => {
+  const [name, setName] = useState("Jorrit"); // Jorrit is default name
+  // Use const since you don't want to redefine the name directly, you should do via setName
+
+  const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
     const names = ["Bob", "Kevin", "Dave"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   };
 
   const handleClick = () => {
-    console.log("You clicked it!");
+    setCount(count + 1);
+    setCount(count + 1);
+    console.log(count);
+    // Some special behavior here! State only changed after function (with +1)
   };
 
   const handleClick2 = (name) => {
-    console.log(`${name} was clicked!`);
-  };
-
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
+    console.log(count);
   };
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+      <p onDoubleClick={handleClick}>Hello {name}</p>
+      <button onClick={handleNameChange}>Change name</button>
       <button onClick={handleClick}>Click it</button>
-      <button onClick={() => handleClick2('Jorrit')}>Click it</button>
-      <button onClick={(e) => handleClick3(e)}>Click it</button>
+      <button onClick={handleClick2}>Click it</button>
     </main>
   );
 };
