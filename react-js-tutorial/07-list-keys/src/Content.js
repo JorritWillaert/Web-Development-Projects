67bf77655c112517fd9dea3886df4c38c7f71a8b
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-01 14:19:20 +0200

    Use an array as state and use buttons for handling a checking event

--- react-js-tutorial/07-list-keys/src/Content.js
+++ react-js-tutorial/07-list-keys/src/Content.js
@@ -1,35 +1,47 @@
-import React from "react";
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
-  const [name, setName] = useState("Jorrit"); // Jorrit is default name
-  // Use const since you don't want to redefine the name directly, you should do via setName
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
 
-  const [count, setCount] = useState(0);
-
-  const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave"];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-  };
-
-  const handleClick = () => {
-    setCount(count + 1);
-    setCount(count + 1);
-    console.log(count);
-    // Some special behavior here! State only changed after function (with +1)
-  };
-
-  const handleClick2 = (name) => {
-    console.log(count);
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
   };
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {name}</p>
-      <button onClick={handleNameChange}>Change name</button>
-      <button onClick={handleClick}>Click it</button>
-      <button onClick={handleClick2}>Click it</button>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked}
+            />
+            <label>{item.item}</label>
+            <FaTrashAlt role="button" tabIndex="0" />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
