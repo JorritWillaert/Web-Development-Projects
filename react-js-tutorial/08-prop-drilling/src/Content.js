3d99591e365a3de36755812226c54c4e3b0162bf
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-01 17:18:09 +0200

    Pass props

--- react-js-tutorial/08-prop-drilling/src/Content.js
+++ react-js-tutorial/08-prop-drilling/src/Content.js
@@ -1,39 +1,6 @@
-import { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-
+const Content = ({ items, setItems, handleCheck, handleDelete }) => {
   return (
     <main>
       {items.length ? (
--- react-js-tutorial/08-prop-drilling/src/Header.js
+++ react-js-tutorial/08-prop-drilling/src/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const Header = () => {
+const Header = ({title}) => {
   return (
     <header>
-      <h1>Groceries List</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
+
+Header.defaultProps = {
+  title: "Default Title"
+}
 
 export default Header;
