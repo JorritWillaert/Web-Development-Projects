1070c623b719c427aa694b88a58c0c4c4998fd2f
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-02 11:47:16 +0200

    Delete from the DB

--- react-js-tutorial/14-crud-operations/src/App.js
+++ react-js-tutorial/14-crud-operations/src/App.js
@@ -76,9 +76,16 @@ function App() {
     }
   };
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
+
+    const deleteOptions = { method: "DELETE" };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) {
+      setFetchError(result);
+    }
   };
 
   const handleSubmit = (e) => {
