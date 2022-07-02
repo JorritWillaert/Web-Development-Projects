b41c4109a7d79ae704941643c34efee9cad55f56
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-02 17:18:17 +0200

    Complete the fetch data challenge (part 1)

--- react-js-tutorial/15-fetch-data-challenge/src/App.js
+++ react-js-tutorial/15-fetch-data-challenge/src/App.js
@@ -1,12 +1,30 @@
 import Form from "./Form";
-import { useState } from "react";
+import List from "./List";
+import { useState, useEffect } from "react";
 
 function App() {
+  const API_URL = "https://jsonplaceholder.typicode.com/";
   const [reqType, setReqType] = useState("users");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${reqType}`);
+        const data = await response.json();
+        setItems(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchItems();
+  }, [reqType]);
 
   return (
     <div className="App">
       <Form reqType={reqType} setReqType={setReqType} />
+      <List items={items} />
     </div>
   );
 }
