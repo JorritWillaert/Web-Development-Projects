e89089a0ecf4133c17fa0c8c1570668ce2a0c848
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-02 11:08:24 +0200

    Catch fetching errors and simulate slow loading data

--- react-js-tutorial/13-fetch-api-data/src/App.js
+++ react-js-tutorial/13-fetch-api-data/src/App.js
@@ -11,20 +11,30 @@ function App() {
   const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchItems = async () => {
       try {
         const response = await fetch(API_URL);
+        if (!response.ok) {
+          throw Error("Did not receive expected data");
+        }
         const listItems = await response.json();
         console.log(listItems);
         setItems(listItems);
+        setFetchError(null);
       } catch (err) {
-        console.log(err.stack);
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
       }
     };
 
+    setTimeout(() => {
     (async () => await fetchItems())();
+    }, 2000)
   }, []);
 
   const addItem = (item) => {
@@ -64,13 +74,19 @@ function App() {
         handleSubmit={handleSubmit}
       />
       <SearchItem search={search} setSearch={setSearch} />
-      <Content
-        items={items.filter((item) =>
-          item.item.toLowerCase().includes(search.toLowerCase())
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter((item) =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
         )}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      </main>
       <Footer length={items.length} />
     </div>
   );
