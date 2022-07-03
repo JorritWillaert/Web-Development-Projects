6372748daddfe3b0b6273c1d05d5d10cba61cc24
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-03 10:06:36 +0200

    Add the three last new components - About, Footer and Missing

--- react-js-tutorial/17-router-hooks-links/src/Footer.js
+++ react-js-tutorial/17-router-hooks-links/src/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Footer = () => {
+  const today = new Date();
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="Footer">
+      <p>Copyright &copy; {today.getFullYear()}</p>
     </footer>
   );
 };
