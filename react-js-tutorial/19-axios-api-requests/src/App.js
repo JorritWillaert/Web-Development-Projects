23d1b4039a48a4b7f45a0899b0638b423a1b33ae
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-03 11:51:51 +0200

    Create an EditPost component

--- react-js-tutorial/19-axios-api-requests/src/App.js
+++ react-js-tutorial/19-axios-api-requests/src/App.js
@@ -2,6 +2,7 @@ import Layout from "./Layout";
 import Home from "./Home";
 import NewPost from "./NewPost";
 import PostPage from "./PostPage";
+import EditPost from "./EditPost";
 import About from "./About";
 import Missing from "./Missing";
 import { Route, Routes, useNavigate } from "react-router-dom";
@@ -15,6 +16,8 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState("");
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -63,6 +66,22 @@ function App() {
     }
   };
 
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: editBody, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(
+        posts.map((post) => (post.id === id ? { ...response.data } : post))
+      );
+      setEditTitle("");
+      setEditBody("");
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
   const handleDelete = async (id) => {
     try {
       await api.delete(`/posts//${id}`);
@@ -99,6 +118,19 @@ function App() {
             element={<PostPage posts={posts} handleDelete={handleDelete} />}
           />
         </Route>
+        <Route
+          path="edit/:id"
+          element={
+            <EditPost
+              posts={posts}
+              handleEdit={handleEdit}
+              editTitle={editTitle}
+              setEditTitle={setEditTitle}
+              editBody={editBody}
+              setEditBody={setEditBody}
+            />
+          }
+        />
         <Route path="about" element={<About />} />
         <Route path="*" element={<Missing />} />
       </Route>
