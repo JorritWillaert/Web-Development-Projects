f910c8e0847128c6cda9db60cb034bb02e4223cd
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-03 11:12:35 +0200

    Add new data to the JSON DB

--- react-js-tutorial/19-axios-api-requests/src/App.js
+++ react-js-tutorial/19-axios-api-requests/src/App.js
@@ -46,16 +46,21 @@ function App() {
     setSearchResults(filteredResults.reverse());
   }, [posts, search]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), "MMMM dd, yyyy pp");
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle("");
-    setPostBody("");
-    navigate("/");
+    try {
+      const response = await api.post('/posts', newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle("");
+      setPostBody("");
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
   };
 
   const handleDelete = (id) => {
