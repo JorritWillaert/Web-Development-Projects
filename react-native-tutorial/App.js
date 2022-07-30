2bb483c732adcc568a751f3f73e876da88931f50
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-30 13:29:16 +0200

    Improve the basic style

--- react-native-tutorial/App.js
+++ react-native-tutorial/App.js
@@ -10,7 +10,7 @@ export default function App() {
         />
         <Button title="Add goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -19,16 +19,26 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: '#ccc'
   },
   TextInput: {
     borderWidth: 1,
     borderColor: "#ccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
   },
+  goalsContainer: {
+    flex: 5
+  }
 });
