9db9ebcdda2b0d32464023ef0b1122c2d97f561b
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-31 10:08:52 +0200

    Create ripple effect on Android

--- react-native-tutorial/components/GoalItem.js
+++ react-native-tutorial/components/GoalItem.js
@@ -2,11 +2,14 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -15,11 +18,11 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
