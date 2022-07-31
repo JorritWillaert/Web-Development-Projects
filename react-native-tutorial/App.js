eac08394877a9cc1621e90ac4294171478513830
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-31 08:45:12 +0200

    Apply a style to the goal item

--- react-native-tutorial/App.js
+++ react-native-tutorial/App.js
@@ -29,7 +29,9 @@ export default function App() {
       </View>
       <View style={styles.goalsContainer}>
         {courseGoals.map((goal) => (
-          <Text key={goal}>{goal}</Text>
+          <Text style={styles.goalItem} key={goal}>
+            {goal}
+          </Text>
         ))}
       </View>
     </View>
@@ -59,5 +61,12 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
   },
 });
