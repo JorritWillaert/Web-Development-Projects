a33fa0acf1adf3b1fab95d820c9b1bb3e8e5c927
JorritWillaert <jorritwillaert98@gmail.com>
2022-07-31 09:22:03 +0200

    Extract the GoalItem component to make the code more modular

--- react-native-tutorial/App.js
+++ react-native-tutorial/App.js
@@ -3,10 +3,10 @@ import {
   Button,
   FlatList,
   StyleSheet,
-  Text,
   TextInput,
   View,
 } from "react-native";
+import GoalItem from './components/GoalItem'
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -38,11 +38,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem text={itemData.item.text} />
           }}
         />
       </View>
@@ -73,14 +69,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-  },
-  goalText: {
-    color: "white",
   },
 });
