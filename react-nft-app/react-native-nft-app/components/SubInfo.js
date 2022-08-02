296ca9521c25a1e12eac7637d7804f947c66c5b3
JorritWillaert <jorritwillaert98@gmail.com>
2022-08-02 19:55:11 +0200

    Add the styling for the EndDate, in order to end the SubInfo component

--- react-nft-app/react-native-nft-app/components/SubInfo.js
+++ react-nft-app/react-native-nft-app/components/SubInfo.js
@@ -46,8 +46,36 @@ export const People = () => {
 
 export const EndDate = () => {
   return (
-    <View>
-      <Text>EndDate</Text>
+    <View
+      style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor: COLORS.white,
+        justifyContent: "center",
+        alignContent: "center",
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: "50%",
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small,
+          color: COLORS.primary,
+        }}
+      >
+        Ending in
+      </Text>
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: SIZES.medium,
+          color: COLORS.primary,
+        }}
+      >
+        12h 30m
+      </Text>
     </View>
   );
 };
