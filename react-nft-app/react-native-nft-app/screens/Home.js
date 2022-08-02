07b876c806c4c2b6936143d603cae4469e15d2c9
JorritWillaert <jorritwillaert98@gmail.com>
2022-08-02 19:03:21 +0200

    List the names of the items in the Home component

--- react-nft-app/react-native-nft-app/screens/Home.js
+++ react-nft-app/react-native-nft-app/screens/Home.js
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import { View, SafeAreaView, FlatList, Text } from "react-native";
 
-import { COLORS, NFTData } from '../constants'
+import { COLORS, NFTData } from "../constants";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+      </View>
     </SafeAreaView>
-  )
+  );
 };
 
 export default Home;
