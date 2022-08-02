b8003bb08318332331316aff6186c11a167df084
JorritWillaert <jorritwillaert98@gmail.com>
2022-08-02 19:28:31 +0200

    Start by making up the NFTCard

--- react-nft-app/react-native-nft-app/components/NFTCard.js
+++ react-nft-app/react-native-nft-app/components/NFTCard.js
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-const NFTCard = () => {
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
   return (
-    <View>
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
+      </View>
       <Text>NFTCard</Text>
     </View>
-  )
-}
+  );
+};
 
-export default NFTCard
+export default NFTCard;
