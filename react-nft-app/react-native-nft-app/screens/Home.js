6677c9fd6cf0880f5bd2c68d814eb171ea249a57
JorritWillaert <jorritwillaert98@gmail.com>
2022-08-02 19:17:57 +0200

    Add some extra Views to the Home component

--- react-nft-app/react-native-nft-app/screens/Home.js
+++ react-nft-app/react-native-nft-app/screens/Home.js
@@ -18,6 +18,18 @@ const Home = () => {
             ListHeaderComponent={<HomeHeader />}
           />
         </View>
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}>
+          <View style={{height: 300, backgroundColor: COLORS.primary}} />
+          <View style={{flex: 1, backgroundColor: COLORS.white}} />
+        </View>
       </View>
     </SafeAreaView>
   );
