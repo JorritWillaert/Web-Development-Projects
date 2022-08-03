710ebe75f01b6e1c9a3c3f16215a7716bb2ea54f
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-03 19:16:40 +0200

    Complete the NFTCard component and make it possible to navigate to the Detail component

--- react-nft-app/react-native-nft-app/components/NFTCard.js
+++ react-nft-app/react-native-nft-app/components/NFTCard.js
@@ -47,8 +47,12 @@ const NFTCard = ({ data }) => {
             alignContent: "center",
           }}
         >
-          <EthPrice price={data.price}/>
-          <RectButton />
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
         </View>
       </View>
     </View>
