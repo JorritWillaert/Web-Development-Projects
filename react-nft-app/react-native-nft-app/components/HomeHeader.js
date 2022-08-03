49c3d671518752693732bc9e04950efe991ab770
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-03 19:31:39 +0200

    Finalize the HomeHeader

--- react-nft-app/react-native-nft-app/components/HomeHeader.js
+++ react-nft-app/react-native-nft-app/components/HomeHeader.js
@@ -45,6 +45,41 @@ const HomeHeader = () => {
           />
         </View>
       </View>
+      <View style={{ marginVertical: SIZES.font }}>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small,
+            color: COLORS.white,
+          }}
+        >
+          Hello Victoria! 👋
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.bold,
+            fontSize: SIZES.large,
+            color: COLORS.white,
+            marginTop: SIZES.base / 2,
+          }}
+        >
+          Let's find a masterpiece
+        </Text>
+      </View>
+      <View style={{ marginTop: SIZES.font }}>
+        <View style={{width: '100%', borderRadius: SIZES.font, backgroundColor: COLORS.gray, flexDirection: 'row', alignItems: 'center', paddingHorizontal: SIZES.font, paddingVertical: SIZES.small - 2}}>
+          <Image
+            source={assets.search}
+            resizeMode="contain"
+            style={{ width: 20, height: 20, marginRight: SIZES.base }}
+          />
+          <TextInput
+            placeholder="Search NFT's"
+            style={{ flex: 1 }}
+            onChangeText={() => {}}
+          />
+        </View>
+      </View>
     </View>
   );
 };
