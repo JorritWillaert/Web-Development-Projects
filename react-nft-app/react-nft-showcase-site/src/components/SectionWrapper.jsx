e27345b709b8fbc2debccc6fbd2de2fc0d272958
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-04 21:22:55 +0200

    Make the SectionWrapper further

--- react-nft-app/react-nft-showcase-site/src/components/SectionWrapper.jsx
+++ react-nft-app/react-nft-showcase-site/src/components/SectionWrapper.jsx
@@ -9,17 +9,20 @@ const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
     <div
       className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}
     >
-      <div className={`flex items-center ${styles.boxClass}`}>
-        <div className={`${styles.descDiv}`}></div>
-        <h1 className={`${styles.h1Text}`}>{title}</h1>
-        <p className={`${styles.descriptionText}`}>
-          <h1>{description}</h1>
-        </p>
-        {showBtn && (
-          <Button assetUrl={assets.expo} link="deployed nft marketplace rn" />
-        )}
+      <div
+        className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}
+      >
+        <div className={`${styles.descDiv}`}>
+          <h1 className={`${styles.h1Text}`}>{title}</h1>
+          <p className={`${styles.descriptionText}`}>{description}</p>
+          {showBtn && (
+            <Button assetUrl={assets.expo} link="deployed nft marketplace rn" />
+          )}
+        </div>
+        <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
+          <img src={mockupImg} alt="mockup" className={styles.sectionImg} />
+        </div>
       </div>
-      {/* <img src={mockupImg} alt="mockup" /> */}
     </div>
   );
 };
