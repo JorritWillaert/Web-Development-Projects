483c401e83f7dfedf3ca54a4ca17e8d05d6e9f2e
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-06 11:09:10 +0200

    Connect the MongoDB database to the app

--- mern-social-media-app/server/index.js
+++ mern-social-media-app/server/index.js
@@ -2,3 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import { passwordMongoDB } from "./secrets.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = `mongodb+srv://JorritWillaert:${passwordMongoDB}@cluster0.ovdo8ut.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
