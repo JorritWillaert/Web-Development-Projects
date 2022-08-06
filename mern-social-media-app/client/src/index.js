fc3cf6f2be5a3ea0aa183b07ea7a579920af8577
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-06 13:52:24 +0200

    Set up redux

--- mern-social-media-app/client/src/index.js
+++ mern-social-media-app/client/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import reducers from "./reducers";
 
 import App from "./App";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDom.render(<App />, document.getElementById("root"));
