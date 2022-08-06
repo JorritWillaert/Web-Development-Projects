e23bdb4b822eeb4db4f5a64c5789d936abb7b6ef
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-06 15:34:21 +0200

    Let the form work to make a post request to our DB

--- mern-social-media-app/client/src/components/Form/Form.js
+++ mern-social-media-app/client/src/components/Form/Form.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
 import useStyles from "./styles.js";
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts.js";
 
 const Form = () => {
   const [postData, setPostData] = useState({
@@ -12,8 +14,12 @@ const Form = () => {
     selectedFile: "",
   });
   const classes = useStyles();
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPost(postData));
+  };
 
   const clear = () => {};
 
