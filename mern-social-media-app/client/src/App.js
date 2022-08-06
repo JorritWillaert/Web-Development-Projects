8e580984769ad475e98e6b9c4addff1c81215a80
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-06 13:25:15 +0200

    Work on basic structure for App.js

--- mern-social-media-app/client/src/App.js
+++ mern-social-media-app/client/src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Form from "./components/Form/Form.js";
+import Posts from "./components/Posts/Posts.js";
 
+import memories from "./images/memories.png";
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
