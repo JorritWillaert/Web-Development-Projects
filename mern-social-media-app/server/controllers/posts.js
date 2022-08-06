a57be0885735c8859edf00ada8a3063c70f86733
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-06 11:29:30 +0200

    Add code to retrieve the posts from the DB

--- mern-social-media-app/server/controllers/posts.js
+++ mern-social-media-app/server/controllers/posts.js
@@ -1,3 +1,15 @@
-export const getPosts = (req, res) => {
-  res.send("This works!");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = (req, res) => {
+  res.send("Post creation");
 };
