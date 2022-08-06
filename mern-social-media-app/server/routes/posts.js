e5fdf6874d13a333e69e4652ddba374e699cce80
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-06 11:22:00 +0200

    Extract the logic of posts in its own controller folder

--- mern-social-media-app/server/routes/posts.js
+++ mern-social-media-app/server/routes/posts.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { getPosts } from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("This works!");
-});
+router.get("/", getPosts);
 
 export default router;
