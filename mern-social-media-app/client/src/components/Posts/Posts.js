658d305d0cb732cdb663b53d7574ea618ba280e7
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-06 15:40:37 +0200

    Add the logic to display all posts in the Posts component

--- mern-social-media-app/client/src/components/Posts/Posts.js
+++ mern-social-media-app/client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post.js";
 import { useSelector } from "react-redux";
 
@@ -10,12 +11,21 @@ const Posts = () => {
 
   console.log(posts);
 
-  return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing="3"
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
