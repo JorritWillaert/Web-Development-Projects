dbfb9e460f9d86a893995cb2145c5cf3ab27fe0c
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-07 09:04:07 +0200

    Fix some bugs

--- mern-social-media-app/client/src/App.js
+++ mern-social-media-app/client/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   useEffect(() => {
     dispatch(getPosts());
-  }, []);
+  }, [currentId, dispatch]);
 
   return (
     <Container maxWidth="lg">
--- mern-social-media-app/client/src/components/Form/Form.js
+++ mern-social-media-app/client/src/components/Form/Form.js
@@ -31,9 +31,19 @@ const Form = ({ currentId, setCurrentId }) => {
     } else {
       dispatch(createPost(postData));
     }
+    clear();
   };
 
-  const clear = () => {};
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({
+      creator: "",
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
+  };
 
   return (
     <Paper className={classes.paper}>
@@ -43,7 +53,9 @@ const Form = ({ currentId, setCurrentId }) => {
         className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
       >
-        <Typography variant="h6">Creating a Memory</Typography>
+        <Typography variant="h6">
+          {currentId ? "Editing" : "Creating"} a Memory
+        </Typography>
         <TextField
           name="creator"
           variant="outlined"
