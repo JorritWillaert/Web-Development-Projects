550fa182e720b59e8131da589dacaf9e8dc8adfd
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-07 11:56:04 +0200

    Create the main form for the sign in and sign up

--- mern-social-media-app/client/src/components/Auth/Auth.js
+++ mern-social-media-app/client/src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Avatar,
   Button,
@@ -8,17 +8,22 @@ import {
   Container,
 } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-
+import Input from "./Input.js";
 import useStyles from "./styles.js";
 
 const Auth = () => {
   const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
 
-  const isSignUp = false;
+  const isSignUp = true;
 
   const handleSubmit = () => {};
 
   const handleChange = () => {};
+
+  const handleShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -29,8 +34,54 @@ const Auth = () => {
         <Typography variant="h5">{isSignUp ? "Sign In" : "Sign In"}</Typography>
         <form className={classes.form} onSubmit={handleSubmit}>
           <Grid container spacing={2}>
-            {isSignUp && <></>}
+            {isSignUp && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  half
+                  autoFocus
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignUp && (
+              <Input
+                name="confirmPassword"
+                label="Repeat password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
           </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSignUp ? "Sign Up" : "Sign In"}
+          </Button>
         </form>
       </Paper>
     </Container>
