55e11f9d1b76a2b4d7325744328ed6631dabbbdc
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-08 19:15:26 +0200

    Finish the frontend logic for now of the login/signup

--- mern-social-media-app/client/src/actions/auth.js
+++ mern-social-media-app/client/src/actions/auth.js
@@ -1,5 +1,22 @@
 import { AUTH } from "../constants/actionTypes.js";
 import * as api from "../api";
 
-export const signin = () => {};
-export const signup = () => {};
+export const signin = (formData, navigate) => async (dispatch) => {
+  try {
+    // Log in user
+
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = (formData, navigate) => async (dispatch) => {
+  try {
+    // Sign up user
+
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
