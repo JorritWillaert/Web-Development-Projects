76e19f1d75ce0e7aa3caccaad89a088a9d460712
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-08 20:26:53 +0200

    Add all the logic for the signup

--- mern-social-media-app/server/controllers/user.js
+++ mern-social-media-app/server/controllers/user.js
@@ -27,4 +27,29 @@ export const signin = async (req, res) => {
   }
 };
 
-export const signup = async (req, res) => {};
+export const signup = async (req, res) => {
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+    if (password !== confirmPassword)
+      return res.status(400).json({ message: "Password's don't match" });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const result = await User.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+    });
+
+    const token = jwt.sign(
+      { email: result.email, id: result._id },
+      "this_is_my_secret",
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ result, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
