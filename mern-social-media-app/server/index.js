521cb31822d3e4fa4e8a7c350936bad7f3a49c87
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-08-08 20:21:54 +0200

    Implement the signin function

--- mern-social-media-app/server/index.js
+++ mern-social-media-app/server/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 import dotenv from "dotenv";
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
