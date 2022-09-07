9c304476122bbe0d8dae3d7263bbb073d6d1b474
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-09-07 19:55:28 +0200

    Store and get favorite numbers

--- web3.0-blockchain-solidity/lesson-3/StorageFactory.sol
+++ web3.0-blockchain-solidity/lesson-3/StorageFactory.sol
@@ -11,4 +11,17 @@ contract StorageFactory {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address
+        // ABI - Application Binary Interface
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex]; 
+        // Because the type is SimpleStorage[] (and not 'address'), we don't have to wrap it in SimpleStorage
+        simpleStorage.store(_simpleStorageNumber); 
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
+    }
 }
