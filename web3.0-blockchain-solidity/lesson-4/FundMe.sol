c3ac1442fb4d24b18d50606241cefdfb58c5f21e
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-09-10 09:34:33 +0200

    Add the withdraw function

--- web3.0-blockchain-solidity/lesson-4/FundMe.sol
+++ web3.0-blockchain-solidity/lesson-4/FundMe.sol
@@ -11,7 +11,7 @@ import "./PriceConverter.sol";
 // Note: contract has a wallet as well
 contract FundMe {
     using PriceConverter for uint256;
-    
+
     uint256 public number;
     uint256 public minimumUsd = 50 * 1e18;
 
@@ -32,7 +32,25 @@ contract FundMe {
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
+    function withdraw() public {
+        /* starting index, ending index, step amount */
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        
+        // Actually withdraw the funds
+        // 1. Transfer 
+        // msg.sender = address; payable(msg.sender) = payable address
+        // payable(msg.sender).transfer(address(this).balance);
 
+        // 2. Send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
 
-    // function withdraw() {}
+        // 3. Call; Recommended way at this time
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
 }
