77edf8559f3532f52e7cfcb8f7a2053e7cea42f0
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-09-10 12:08:35 +0200

    Add some more code for working locally with the blockchain

--- web3.0-blockchain-solidity/lesson-5/deploy.js
+++ web3.0-blockchain-solidity/lesson-5/deploy.js
@@ -1,4 +1,21 @@
-async function main() {}
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
+  // http://127.0.0.1:7545
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http:////127.0.0.1:7545"
+  );
+  const privateKey = new ethers.Wallet.fromMnemonic(
+    "cousin across swim harvest obtain perfect crawl huge lock muffin survey parade"
+  ).privateKey; // Sharing this menmonic is fine, it's just locally used for testing
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+}
 
 main()
   .then(() => process.exit(0))
