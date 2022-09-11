2199a29479e4126a60005c952199a39684097b4c
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-09-11 11:03:15 +0200

    Deploy contract to Goerli

--- web3.0-blockchain-solidity/lesson-6/hardhat.config.js
+++ web3.0-blockchain-solidity/lesson-6/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  // Will automatically use 'defaultNetwork: "hardhat"'
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
   solidity: "0.8.8",
 };
