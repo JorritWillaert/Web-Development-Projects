cadd1571a2281a9453c9c12ed596d7c1771595c0
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-10-08 20:49:36 +0200

    Add static JSX for HeroBanner

--- nextjs-react-ecommerce/ecommerce/pages/index.js
+++ nextjs-react-ecommerce/ecommerce/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import { Product, FooterBanner, HeroBanner } from "../components";
+
 const Home = () => {
   return (
     <>
-      HereBanner
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Sneakers of many variations</p>
@@ -11,7 +13,7 @@ const Home = () => {
       <div className="products-container">
         {["Product 1", "Product2"].map((product) => product)}
       </div>
-      Footer
+      <FooterBanner />
     </>
   );
 };
