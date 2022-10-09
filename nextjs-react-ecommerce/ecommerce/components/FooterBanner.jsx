2510ed870992f72a3b250799f3f280ea5b46ebdc
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-10-09 19:33:07 +0200

    Add general outline for footer component

--- nextjs-react-ecommerce/ecommerce/components/FooterBanner.jsx
+++ nextjs-react-ecommerce/ecommerce/components/FooterBanner.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import Link from "next/link";
 
-const FooterBanner = () => {
-  return <div>FooterBanner</div>;
+import { urlFor } from "../lib/client.js";
+
+const FooterBanner = ({
+  footerBanner: {
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    product,
+    buttonText,
+    image,
+  },
+}) => {
+  return (
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+
+          <img src={urlFor(image)} className="footer-banner-image" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FooterBanner;
--- nextjs-react-ecommerce/ecommerce/pages/index.js
+++ nextjs-react-ecommerce/ecommerce/pages/index.js
@@ -16,7 +16,7 @@ const Home = ({ products, bannerData }) => {
           <Product key={product._id} product={product} />
         ))}
       </div>
-      <FooterBanner />
+      <FooterBanner FooterBanner={bannerData && bannerData[0]} />
     </>
   );
 };
