a63118ab3342053e2796e2ec1aff24f445de4864
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-10-09 19:24:51 +0200

    Make the products display

--- nextjs-react-ecommerce/ecommerce/components/Product.jsx
+++ nextjs-react-ecommerce/ecommerce/components/Product.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Link from "next/link";
 
-const Product = () => {
-  return <div>Product</div>;
+import { urlFor } from "../lib/client.js";
+
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Product;
