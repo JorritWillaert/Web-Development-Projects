5de9ea0fbb3a13911c14ea47be8274930c54fa9c
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-10-11 19:21:13 +0200

    Fix cart outline

--- nextjs-react-ecommerce/ecommerce/components/Cart.jsx
+++ nextjs-react-ecommerce/ecommerce/components/Cart.jsx
@@ -46,6 +46,42 @@ const Cart = () => {
             </Link>
           </div>
         )}
+
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item, index) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>${item.price}</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span className="minus" onClick="">
+                          <AiOutlineMinus />
+                        </span>
+                        <span className="num" onClick="">
+                          0
+                        </span>
+                        <span className="plus" onClick="">
+                          <AiOutlinePlus />
+                        </span>
+                      </p>
+                    </div>
+                    <button type="button" className="remove-item" onClick="">
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
