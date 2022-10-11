c9325d268d0b9df3ec62a95bbfb36b4d4c65ff61
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-10-11 20:28:50 +0200

    Add some extra optionss, and add 2 shipping rates

--- nextjs-react-ecommerce/ecommerce/pages/api/stripe.js
+++ nextjs-react-ecommerce/ecommerce/pages/api/stripe.js
@@ -8,6 +8,14 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1LrnAfCfv7D31UEOvvSZa0zY" },
+          { shipping_rate: "shr_1LrnBJCfv7D31UEOEzDfnhOe" },
+        ],
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
