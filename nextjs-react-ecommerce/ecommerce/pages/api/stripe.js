099ce0c9810a53701d119443b9352657880b0987
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-10-11 20:25:20 +0200

    Add basic part of Stripe

---
+++ nextjs-react-ecommerce/ecommerce/pages/api/stripe.js
@@ -0,0 +1,32 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+// Note: since the file is called `stripe.js`, the route is `/stripe`
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const params = {
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: "{{PRICE_ID}}",
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
