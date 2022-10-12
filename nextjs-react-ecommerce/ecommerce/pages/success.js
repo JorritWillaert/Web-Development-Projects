6a4e9a64b931d9ae8642d191e2b2b346a86e6a7b
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-10-12 14:52:33 +0200

    Add JSX for success page

--- nextjs-react-ecommerce/ecommerce/pages/success.js
+++ nextjs-react-ecommerce/ecommerce/pages/success.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
-import { useRouter } from "next/router";
 
-import { useStateContext } from "../../context/StateContext";
+import { useStateContext } from "../context/StateContext";
 
 const Success = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
-  const [order, setOrder] = useState(null);
 
   return (
     <div className="success-wrapper">
@@ -15,6 +13,17 @@ const Success = () => {
         <p className="icon">
           <BsBagCheckFill />
         </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="description">
+          If you have any questions, please email us.
+          {/* TODO add mail with anchor text and mailto */}
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
       </div>
     </div>
   );
