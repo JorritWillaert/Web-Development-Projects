c6256f3ab68ff030cb726b158885ef4aff645ffa
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-02 21:00:14 +0100

    Add more for the Header and start with the icons

--- react-instagram-clone/components/Header.js
+++ react-instagram-clone/components/Header.js
@@ -8,11 +8,12 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
     <div>
-      <div className="flex justify-between max-w-6xl">
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* Left */}
         <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
@@ -31,19 +32,25 @@ function Header() {
         </div>
 
         {/* Middle ~ Search input field */}
-        <div className="relative mt-1 p-3 rounded-md">
-          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-            <SearchIcon className="h-5 w-5 text-gray-500" />
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
           </div>
-          <input
-            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray focus:ring-black focus:border-black"
-            type="text"
-            placeholder="Search"
-          />
         </div>
 
         {/* Right */}
-      </div>{" "}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="h-10 w-10" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+        </div>
+      </div>
     </div>
   );
 }
