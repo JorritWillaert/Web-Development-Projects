b760686c9391c2b3977f1b043e250e15b4cab134
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-02 22:53:58 +0100

    Customize scroll bar

--- react-instagram-clone/components/Story.js
+++ react-instagram-clone/components/Story.js
@@ -3,8 +3,12 @@ import React from "react";
 const Story = ({ img, username }) => {
   return (
     <div>
-      <img src={img} alt="Avatar" />
-      <p>{username}</p>
+      <img
+        className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transition transform duration-200"
+        src={img}
+        alt="Avatar"
+      />
+      <p className="text-xs w-14 truncate text-center">{username}</p>
     </div>
   );
 };
--- react-instagram-clone/pages/index.tsx
+++ react-instagram-clone/pages/index.tsx
@@ -5,7 +5,7 @@ import Feed from "../components/Feed";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram 2.0 Youtube</title>
         <link rel="icon" href="/favicon.ico" />
