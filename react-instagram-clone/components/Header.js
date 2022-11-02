2e8d4010bec9de6de34bd3fbdf2d12ef5e5d9976
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-02 20:24:01 +0100

    Start styling the Header

---
+++ react-instagram-clone/components/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Image from "next/image.js";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <div>
+      <div className="flex justify-between max-w-6xl">
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* Middle ~ Search input field */}
+        <div className="relative mt-1 p-3 rounded-md">
+          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+            <SearchIcon className="h-5 w-5 text-gray-500" />
+          </div>
+          <input
+            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray focus:ring-black focus:border-black"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+
+        {/* Right */}
+      </div>{" "}
+    </div>
+  );
+}
+
+export default Header;
--- react-instagram-clone/pages/index.tsx
+++ react-instagram-clone/pages/index.tsx
@@ -1,15 +1,21 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Instagram 2.0 Youtube</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>This is Instagram 2.0</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Feed */}
+
+      {/* Modal */}
     </div>
   );
 };
