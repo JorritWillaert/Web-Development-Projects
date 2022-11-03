b2e1a22603c8bfa9662f9b0d02ba5757a1534054
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-03 13:20:26 +0100

    Work more on Post

--- react-instagram-clone/components/Post.js
+++ react-instagram-clone/components/Post.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { DotsHorizontalIcon } from "@heroicons/react/outline";
 
-const Post = () => {
-  return <div>Post</div>;
+const Post = ({ id, username, userImg, img, caption }) => {
+  return (
+    <div>
+      {/* Header */}
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          alt="user"
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+        />
+        <p>{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      {/* img */}
+
+      {/* Buttons */}
+
+      {/* Caption */}
+
+      {/* Comments */}
+
+      {/* Input box */}
+    </div>
+  );
 };
 
 export default Post;
