e314593f4cfd30487539cd0c05e0fe92b2bcdf65
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-03 14:22:01 +0100

    Start on suggestions

--- react-instagram-clone/components/Suggestions.js
+++ react-instagram-clone/components/Suggestions.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { randUserName } from "@ngneat/falso";
 
 const Suggestions = () => {
-  return <div>Suggestions</div>;
+  const [suggestions, setSuggestions] = useState(null);
+
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => ({
+      username: randUserName(),
+      avatar: "https://i.pravatar.cc/100/" + i,
+      id: i,
+    }));
+
+    setSuggestions(suggestions);
+  }, []);
+
+  return (
+    <div className="mt-4 ml-10">
+      <div className="flex justify-between text-sm mb-5">
+        <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
+        <button className="text-gray-600 font-semibold">See All</button>
+      </div>
+    </div>
+  );
 };
 
 export default Suggestions;
