ba0e4799a556e8e3793a4f225f8474532d308c96
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-03 13:29:42 +0100

    Add icons and style them

--- react-instagram-clone/components/Post.js
+++ react-instagram-clone/components/Post.js
@@ -1,23 +1,36 @@
 import React from "react";
-import { DotsHorizontalIcon } from "@heroicons/react/outline";
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
 const Post = ({ id, username, userImg, img, caption }) => {
   return (
-    <div>
-      {/* Header */}
+    <div className="bg-white my-7 border rounded-sm">
       <div className="flex items-center p-5">
         <img
           src={userImg}
           alt="user"
           className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
         />
-        <p>{username}</p>
+        <p className="flex-1 font-bold">{username}</p>
         <DotsHorizontalIcon className="h-5" />
       </div>
 
-      {/* img */}
+      <img src={img} className="object-cover w-full" alt="post" />
 
-      {/* Buttons */}
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
 
       {/* Caption */}
 
