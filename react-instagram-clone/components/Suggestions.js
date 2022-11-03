a5c7e07655e8250707bf579c0ceadee315c47a5f
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-03 14:28:38 +0100

    Finalize suggestions

--- react-instagram-clone/components/Suggestions.js
+++ react-instagram-clone/components/Suggestions.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { randUserName } from "@ngneat/falso";
+import { randUserName, randBrand } from "@ngneat/falso";
 
 const Suggestions = () => {
   const [suggestions, setSuggestions] = useState(null);
@@ -7,6 +7,7 @@ const Suggestions = () => {
   useEffect(() => {
     const suggestions = [...Array(5)].map((_, i) => ({
       username: randUserName(),
+      company: randBrand(),
       avatar: "https://i.pravatar.cc/100/" + i,
       id: i,
     }));
@@ -20,6 +21,30 @@ const Suggestions = () => {
         <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
         <button className="text-gray-600 font-semibold">See All</button>
       </div>
+
+      {suggestions?.map((profile) => (
+        <div
+          key={profile.id}
+          className="flex items-center justify-between mt-3"
+        >
+          <img
+            src={profile.avatar}
+            alt="user"
+            className="rounded-full w-10 h-10 border p-[2px]"
+          />
+
+          <div className="flex-1 ml-4">
+            <h2 className="font-semibold text-sm">{profile.username}</h2>
+            <h3 className="text-xs text-gray-400">
+              Works at {profile.company}
+            </h3>
+          </div>
+
+          <button className="text-blue-400 text-xs font-semibold">
+            Follow
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
