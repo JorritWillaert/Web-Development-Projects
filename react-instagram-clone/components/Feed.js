927091c47b0dc00f7514f71620ee0a701626ede1
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-04 16:10:10 +0100

    Fix further things if no session is present

--- react-instagram-clone/components/Feed.js
+++ react-instagram-clone/components/Feed.js
@@ -3,21 +3,30 @@ import Stories from "./Stories.js";
 import Posts from "./Posts.js";
 import MiniProfile from "./MiniProfile.js";
 import Suggestions from "./Suggestions.js";
+import { useSession } from "next-auth/react";
 
 const Feed = () => {
+  const { data: session } = useSession();
+
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+    <main
+      className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${
+        !session && "!grid-cols-1 !max-w-3xl"
+      }`}
+    >
       <section className="col-span-2">
         <Stories />
         <Posts />
       </section>
 
-      <section className="hidden xl:inline-grid md:col-span-1">
-        <div className="fixed top-20">
-          <MiniProfile />
-          <Suggestions />
-        </div>
-      </section>
+      {session && (
+        <section className="hidden xl:inline-grid md:col-span-1">
+          <div className="fixed top-20">
+            <MiniProfile />
+            <Suggestions />
+          </div>
+        </section>
+      )}
     </main>
   );
 };
--- react-instagram-clone/components/MiniProfile.js
+++ react-instagram-clone/components/MiniProfile.js
@@ -4,24 +4,24 @@ import { signOut, useSession } from "next-auth/react";
 const MiniProfile = () => {
   const { data: session } = useSession();
 
-  console.log(session);
-
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <div>
         <img
-          src="https://links.papareact.com/3ke"
+          src={session?.user?.image}
           alt="user"
           className="rounded-full border p-[2px] w-16 h-16"
         />
       </div>
 
       <div className="flex-1 mx-4">
-        <h2 className="font-bold">Testname</h2>
+        <h2 className="font-bold">{session?.user?.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
 
-      <button className="text-blue-400 text-sm font-semibold">Sign Out</button>
+      <button onClick={signOut} className="text-blue-400 text-sm font-semibold">
+        Sign Out
+      </button>
     </div>
   );
 };
