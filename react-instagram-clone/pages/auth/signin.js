d9a9f9714afb507229628e531a006799fe26e832
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-04 13:36:49 +0100

    Customize signin page

--- react-instagram-clone/pages/auth/signin.js
+++ react-instagram-clone/pages/auth/signin.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { getProviders, signIn as signIntoProvider } from "next-auth/react";
+import Header from "../../components/Header";
 
 // On the browser...
 const signIn = ({ providers }) => {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+        <img
+          className="w-80"
+          src="https://links.papareact.com/ocw"
+          alt="Instagram"
+        />
+        <p>This is a testing app</p>
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() => signIntoProvider(provider.id)}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
