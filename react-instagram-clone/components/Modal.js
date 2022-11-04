2ecc90dabc51601aac173febab10ac0369443005
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-04 17:10:05 +0100

    Add icons and text to modal

--- react-instagram-clone/components/Modal.js
+++ react-instagram-clone/components/Modal.js
@@ -3,6 +3,7 @@ import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom.js";
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
+import { CameraIcon } from "@heroicons/react/outline";
 
 const Modal = () => {
   const [open, setOpen] = useRecoilState(modalState);
@@ -46,13 +47,44 @@ const Modal = () => {
           >
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
               <div>
-                <div className="mt-5 sm:mt-6">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
-                  >
-                    Upload Post
-                  </button>
+                <div
+                  onClick={() => filePickerRef.current.click()}
+                  className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
+                >
+                  <CameraIcon
+                    className="h-6 w-6 text-red-600"
+                    aria-hidden="true"
+                  />
+                </div>
+                <div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      Upload a photo
+                    </Dialog.Title>
+
+                    <div>
+                      <input type="file" hidden />
+                      <div className="mt-2">
+                        <input
+                          className="border-none focus:ring-0 w-full text-center"
+                          type="text"
+                          placeholder="Please enter a caption..."
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mt-5 sm:mt-6">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                    >
+                      Upload Post
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
