19e7021806448aad381da0bd19ad14fc8af9880e
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-04 16:17:53 +0100

    Add story icon for profile logged in

--- react-instagram-clone/components/Stories.js
+++ react-instagram-clone/components/Stories.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { randUserName } from "@ngneat/falso";
 import Story from "./Story.js";
+import { useSession } from "next-auth/react";
 
 const Stories = () => {
   const [suggestions, setSuggestions] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
@@ -17,6 +19,10 @@ const Stories = () => {
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story img={session.user.image} username={session.user.username} />
+      )}
+
       {suggestions.map((profile) => (
         <Story
           key={profile.id}
