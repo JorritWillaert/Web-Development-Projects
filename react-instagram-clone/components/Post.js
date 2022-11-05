b728e5c12c25283add9fc5c9c51f1292ffec75e4
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-05 19:06:34 +0100

    Add functionality for comments

--- react-instagram-clone/components/Post.js
+++ react-instagram-clone/components/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BookmarkIcon,
   ChatIcon,
@@ -9,9 +9,27 @@ import {
 } from "@heroicons/react/outline";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 import { useSession } from "next-auth/react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase.js";
 
 const Post = ({ id, username, userImg, img, caption }) => {
   const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+    const commentToSend = comment;
+    setComment("");
+
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
+
   return (
     <div className="bg-white my-7 border rounded-sm">
       <div className="flex items-center p-5">
@@ -49,10 +67,19 @@ const Post = ({ id, username, userImg, img, caption }) => {
           <EmojiHappyIcon className="h-7" />
           <input
             type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
             placeholder="Add a comment..."
             className="border-none flex-1 focus:ring-0 outline-none"
           />
-          <button className="font-semibold text-blue-400">Post</button>
+          <button
+            className="font-semibold text-blue-400"
+            type="submit"
+            disabled={!comment.trim()}
+            onClick={sendComment}
+          >
+            Post
+          </button>
         </form>
       )}
     </div>
