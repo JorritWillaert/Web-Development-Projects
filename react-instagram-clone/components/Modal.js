5509f6de2b7b5267d24cd9e60fc816ff264f5999
Jorrit Willaert <jorritwillaert98@gmail.com>
2022-11-05 17:20:12 +0100

    Get the uploading working

--- react-instagram-clone/components/Modal.js
+++ react-instagram-clone/components/Modal.js
@@ -12,6 +12,7 @@ import {
   updateDoc,
 } from "firebase/firestore";
 import { ref, getDownloadURL, uploadString } from "firebase/storage";
+import { useSession } from "next-auth/react";
 
 const Modal = () => {
   const { data: session } = useSession();
@@ -151,9 +152,11 @@ const Modal = () => {
                   <div className="mt-5 sm:mt-6">
                     <button
                       type="button"
+                      disabled={!selectedFile || loading}
                       className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                      onClick={uploadPost}
                     >
-                      Upload Post
+                      {loading ? "Uploading..." : "Upload Post"}
                     </button>
                   </div>
                 </div>
