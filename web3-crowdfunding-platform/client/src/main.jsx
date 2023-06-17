e7150427278686b8a50f105098d405387a0ca3b3
Jorrit Willaert <jorritwillaert98@gmail.com>
2023-06-17 13:43:06 +0200

    Add some assets + tailwind

--- web3-crowdfunding-platform/client/src/main.jsx
+++ web3-crowdfunding-platform/client/src/main.jsx
@@ -5,9 +5,6 @@ import { Sepolia } from "@thirdweb-dev/chains";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
