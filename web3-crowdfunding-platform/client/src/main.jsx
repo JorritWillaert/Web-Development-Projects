1d547ef924dbe783b9435576b437744f38f2c610
Jorrit Willaert <jorritwillaert98@gmail.com>
2023-06-17 13:37:45 +0200

    Get the app to work

--- web3-crowdfunding-platform/client/src/main.jsx
+++ web3-crowdfunding-platform/client/src/main.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import { Sepolia } from "@thirdweb-dev/chains";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
+  <ThirdwebProvider activeChain={Sepolia}>
+    <Router>
       <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
+    </Router>
+  </ThirdwebProvider>
 );
