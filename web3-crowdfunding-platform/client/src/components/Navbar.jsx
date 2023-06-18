abd6bfec343c152def8e05103a4a8897f132a80e
Jorrit Willaert <jorritwillaert98@gmail.com>
2023-06-18 10:10:08 +0200

    Add further parts of the NavBar

--- web3-crowdfunding-platform/client/src/components/Navbar.jsx
+++ web3-crowdfunding-platform/client/src/components/Navbar.jsx
@@ -10,6 +10,8 @@ const Navbar = () => {
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
 
+  const address = "0x1234567890123456789012345678901234567890";
+
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-35[px] gap-6">
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
@@ -27,6 +29,28 @@ const Navbar = () => {
           />
         </div>
       </div>
+
+      <div className="sm:flex hidden flex-row justify-end gap-4">
+        <CustomButton
+          btnType="button"
+          title={address ? "Create a campaign" : "Connect wallet"}
+          styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+          handleClick={() => {
+            if (address) navigate("create-campaign");
+            else "connect()";
+          }}
+        />
+      </div>
+
+      <Link to="/profile">
+        <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+          <img
+            src={thirdweb}
+            alt="user"
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+      </Link>
     </div>
   );
 };
