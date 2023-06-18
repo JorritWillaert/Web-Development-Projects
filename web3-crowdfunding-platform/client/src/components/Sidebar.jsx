a715dd9c00dc2e630700f5ed2ba03de6e939d6be
Jorrit Willaert <jorritwillaert98@gmail.com>
2023-06-18 09:34:17 +0200

    First part of sidebar

--- web3-crowdfunding-platform/client/src/components/Sidebar.jsx
+++ web3-crowdfunding-platform/client/src/components/Sidebar.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { logo, sun } from "../assets";
+import { navlinks } from "../constants/index.js";
+
+const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
+  <div
+    className={`w-[48px] h-[48px] rounded-[10px] ${
+      isActive && isActive === name && "bg-[#2c3f32]"
+    } flex justify-center items-center ${
+      !disabled && "cursor-pointer"
+    } ${styles}
+    `}
+    onClick={handleClick}
+  >
+    {!isActive ? (
+      <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+    ) : (
+      <img
+        src={imgUrl}
+        alt="fund_logo"
+        className="w-1/2 h-1/2 ${isActive !== name && 'grayscale'}"
+      />
+    )}
+  </div>
+);
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
+
+  return (
+    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+      <Link to="/">
+        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+      </Link>
+      Sidebar
+    </div>
+  );
 };
 
 export default Sidebar;
