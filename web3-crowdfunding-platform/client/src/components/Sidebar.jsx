6032083dfabb0f21856007d8845b1458de85af67
Jorrit Willaert <jorritwillaert98@gmail.com>
2023-06-18 09:41:34 +0200

    Finalize Sidebar

--- web3-crowdfunding-platform/client/src/components/Sidebar.jsx
+++ web3-crowdfunding-platform/client/src/components/Sidebar.jsx
@@ -35,7 +35,25 @@ const Sidebar = () => {
       <Link to="/">
         <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
       </Link>
-      Sidebar
+
+      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
+        <div className="flex flex-col justify-center items-center gap-3">
+          {navlinks.map((link) => (
+            <Icon
+              key={link.name}
+              {...link}
+              isActive={isActive}
+              handleClick={() => {
+                if (!link.disabled) {
+                  setIsActive(link.name);
+                  navigate(link.link);
+                }
+              }}
+            />
+          ))}
+        </div>
+        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+      </div>
     </div>
   );
 };
