c2ae7f73707d4cb7403056378435974863e0e6bd
Jorrit Willaert <jorritwillaert98@gmail.com>
2023-06-18 09:11:59 +0200

    Create some basic components

--- web3-crowdfunding-platform/client/src/main.jsx
+++ web3-crowdfunding-platform/client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { Sepolia } from "@thirdweb-dev/chains";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
