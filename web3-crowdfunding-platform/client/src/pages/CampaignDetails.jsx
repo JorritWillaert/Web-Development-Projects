1d953a2d5e9a911edcf98c6f93870d190e5508dc
Jorrit Willaert <jorritwillaert98@gmail.com>
2023-06-19 14:28:48 +0200

    Visualize the donators as well

--- web3-crowdfunding-platform/client/src/pages/CampaignDetails.jsx
+++ web3-crowdfunding-platform/client/src/pages/CampaignDetails.jsx
@@ -113,7 +113,19 @@ const CampaignDetails = () => {
             </h4>
             <div className="mt-[20px] flex flex-col gap-4">
               {donators.length > 0 ? (
-                donators.map((item, index) => <div>DONATOR</div>)
+                donators.map((item, index) => (
+                  <div
+                    key={`${item.donator}-${index}`}
+                    className="flex justify-between items-center gap-4"
+                  >
+                    <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-all">
+                      {index + 1}. {item.donator}
+                    </p>
+                    <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-all">
+                      {item.donation}
+                    </p>
+                  </div>
+                ))
               ) : (
                 <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">
                   No donators yet. Be the first one!
