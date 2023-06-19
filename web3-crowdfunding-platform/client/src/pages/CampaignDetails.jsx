8967ec100fdf01ad5dbd7437938fad7d2ca49986
Jorrit Willaert <jorritwillaert98@gmail.com>
2023-06-19 13:58:34 +0200

    Finalize the frontend of Campaign page

--- web3-crowdfunding-platform/client/src/pages/CampaignDetails.jsx
+++ web3-crowdfunding-platform/client/src/pages/CampaignDetails.jsx
@@ -17,6 +17,8 @@ const CampaignDetails = () => {
 
   const remainingDays = daysLeft(state.deadline);
 
+  const handleDonate = async () => {};
+
   return (
     <div>
       {isLoading && "Loading..."}
@@ -101,6 +103,45 @@ const CampaignDetails = () => {
                   No donators yet. Be the first one!
                 </p>
               )}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1">
+          <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
+            Fund
+          </h4>
+
+          <div className="mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]">
+            <p className="font-epilogue font-medium text-[20px] leading-[30px] text-center text-[#808191]">
+              Fund the campaign
+            </p>
+            <div className="mt-[30px]">
+              <input
+                type="number"
+                placeholder="ETH 0.1"
+                step="0.01"
+                className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+
+              <div className="my-[20px] p-4 bg-[#13131a] rounded-[10px]">
+                <h4 className="font-epilogue font-semibold text-[14px] leading-[22px] text-white">
+                  Back it because you believe in it.
+                </h4>
+                <p className="mt-[20px] font-epilogue font-normal leading-[22px] text-[#808191]">
+                  Support the project for no reward, just because it speaks to
+                  you.
+                </p>
+              </div>
+
+              <CustomButton
+                btnType="button"
+                title="Fund Campaign"
+                styles="w-full bg-[#8c6dfd]"
+                handleClick={handleDonate}
+              />
             </div>
           </div>
         </div>
