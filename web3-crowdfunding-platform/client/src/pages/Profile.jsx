a4c65b4bfd92ace5be91942ead6ea8f32cf0c6eb
Jorrit Willaert <jorritwillaert98@gmail.com>
2023-06-19 12:49:56 +0200

    Use the same components to show own campaigns

--- web3-crowdfunding-platform/client/src/pages/Profile.jsx
+++ web3-crowdfunding-platform/client/src/pages/Profile.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getUserCampaigns } = useStateContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) {
+      fetchCampaigns();
+    }
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    ></DisplayCampaigns>
+  );
 };
 
 export default Profile;
