39054fcacaf30021b5d63e8d1907ba11792fc196
Jorrit Willaert <jorritwillaert98@gmail.com>
2023-06-19 13:07:18 +0200

    Start developing the CampaignDetails

--- web3-crowdfunding-platform/client/src/pages/CampaignDetails.jsx
+++ web3-crowdfunding-platform/client/src/pages/CampaignDetails.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { ethers } from "ethers";
+
+import { useStateContext } from "../context";
+import { CountBox, CustomButton } from "../components";
+import { calculateBarPercentage, daysLeft } from "../utils";
+import { thirdweb } from "../assets";
 
 const CampaignDetails = () => {
-  return <div>CampaignDetails</div>;
+  const { state } = useLocation();
+  const { getDonations, contract, address } = useStateContext();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [amount, setAmount] = useState("");
+  const [donators, setDonators] = useState([]);
+
+  const remainingDays = daysLeft(state.deadline);
+
+  return (
+    <div>
+      {isLoading && "Loading..."}
+
+      <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+        <div className="flex-1 flex-col">
+          <img
+            src={state.image}
+            alt="campaign"
+            className="w-full h-[410[x] object-cover rounded-xl"
+          />
+          <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
+            <div
+              className="absolute h-full bg-[#4acd8c]"
+              style={{
+                widht: `${calculateBarPercentage(
+                  state.target,
+                  state.amountCollected
+                )}%`,
+                maxWidth: "100%",
+              }}
+            ></div>
+          </div>
+        </div>
+
+        <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
+          <CountBox title="Days Left" value={remainingDays} />
+          <CountBox
+            title={`Raised of ${state.target}`}
+            value={state.amountCollected}
+          />
+          <CountBox title="Total Backers" value={donators.length} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CampaignDetails;
